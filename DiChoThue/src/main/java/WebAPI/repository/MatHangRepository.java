660c2c29811d71c515b72ca409c5beb5ea76a5ea
QuocTuan18CLC1 <QuocTuan18CLC1>
2022-01-17 01:16:15 +0700

    reveal hinh_anh column

--- DiChoThue/src/main/java/WebAPI/repository/MatHangRepository.java
+++ DiChoThue/src/main/java/WebAPI/repository/MatHangRepository.java
@@ -12,7 +12,7 @@ public interface MatHangRepository extends MongoRepository<MatHang, String> {
 	//CRUD
 	
 	@Query(value="{ten_mat_hang:{$regex:?0}}", fields="{'ten_mat_hang' : 1,"
-			+ "'khoi_luong' : 1, 'gia_ban' : 1, 'ma_cua_hang' : 1}")
+			+ "'khoi_luong' : 1, 'gia_ban' : 1, 'ma_cua_hang' : 1, 'hinh_anh': 1}")
 	List<MatHang> TimTheoTen(String name);
 	
 	@Query(value="{}", fields="{'ten_mat_hang' : 1, 'khoi_luong' : 1,"
